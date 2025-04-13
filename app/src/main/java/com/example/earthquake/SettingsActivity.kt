bab794af7d5932ffeee04e12a7960bd9c41e5af9
najwansv <najwan.joan.sv@gmail.com>
2025-04-13 12:43:20 +0700

    update layout emergency

--- app/src/main/java/com/example/earthquake/SettingsActivity.kt
+++ app/src/main/java/com/example/earthquake/SettingsActivity.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
